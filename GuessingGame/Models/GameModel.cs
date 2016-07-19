de5b845e0fc1c25c6e4f55ecb63323653330828a
MitchReed123 <reed.mitch@live.com>
2016-07-19 09:45:40 -0400

    Here

---
+++ GuessingGame/Models/GameModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GuessingGame.Models
+{
+    public class GameModel
+    {
+        [Display(Name = "Enter Your Name")]
+        [Required(ErrorMessage = "You must enter your name")]
+
+        public string PlayerName { get; set; }
+
+        [Display(Name = "What is your guess")]
+        [Required(ErrorMessage = "Guess is required")]
+        public int Guess { get; set; }
+
+        public override string ToString()
+        {
+            return $"{PlayerName} ({Guess})";
+        }
+    }
+}
